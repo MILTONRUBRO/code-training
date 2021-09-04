b29d162a99c0d90bb8e906c6408cc81314b3cfea
Milton <miltonrubro@gmail.com>
2021-09-04 16:15:44 -0300

    Criando projeto

---
+++ src/main/java/br/com/devmos/model/Car.java
@@ -0,0 +1,23 @@
+package br.com.devmos.model;
+
+import java.util.Objects;
+
+public class Car {
+
+	private String cor;
+	private String modelo;
+
+	public Car(String cor, String modelo) {
+		this.cor = Objects.requireNonNull(cor, "A cor do carro não pode ser nulla");
+		this.modelo = Objects.requireNonNull(modelo, "O modelo do carro não pode ser nullo");
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+}
