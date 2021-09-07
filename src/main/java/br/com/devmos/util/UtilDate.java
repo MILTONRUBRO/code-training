9245c7689072b6df41c071720e641792f6dee563
Milton <miltonrubro@gmail.com>
2021-09-07 17:23:36 -0300

    Fazendo sobrecarga no metodo stringToDate com formmater de data

--- src/main/java/br/com/devmos/util/UtilDate.java
+++ src/main/java/br/com/devmos/util/UtilDate.java
@@ -8,5 +8,10 @@ public class UtilDate {
 	public static LocalDate stringToDate(String data) {
 		return LocalDate.parse(data, DateTimeFormatter.ofPattern("d-MM-yyyy"));
 	}
+	
+	public static LocalDate stringToDate(String data, DateTimeFormatter formatter) {
+		return LocalDate.parse(data, formatter);
+	}
+
 
 }
--- src/test/java/br/com/devmos/util/UtilDateTest.java
+++ src/test/java/br/com/devmos/util/UtilDateTest.java
@@ -3,16 +3,30 @@ package br.com.devmos.util;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.swing.text.DateFormatter;
 
 import org.junit.Test;
 
 public class UtilDateTest {
 
 	private static final String STRING_DATE = "06-09-2021";
+	private static final String STRING_DATE_ISO = "2021-09-06";
 
 	@Test
 	public void testStringToDateOk() {
 		LocalDate date = UtilDate.stringToDate(STRING_DATE);
+
+		assertEquals(6, date.getDayOfMonth());
+		assertEquals(9, date.getMonth().getValue());
+		assertEquals(2021, date.getYear());
+	}
+	
+	@Test
+	public void testStringToDateWithFormatter() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
+		LocalDate date = UtilDate.stringToDate(STRING_DATE_ISO, formatter);
 
 		assertEquals(6, date.getDayOfMonth());
 		assertEquals(9, date.getMonth().getValue());
