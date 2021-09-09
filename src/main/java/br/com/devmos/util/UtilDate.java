f738f5b2255e4912841ff06dcfdb5b6fe09776d8
Milton <miltonrubro@gmail.com>
2021-09-09 19:16:31 -0300

    Adiconando metodo para calculo de idade

--- src/main/java/br/com/devmos/util/UtilDate.java
+++ src/main/java/br/com/devmos/util/UtilDate.java
@@ -1,6 +1,8 @@
 package br.com.devmos.util;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 public class UtilDate {
@@ -12,6 +14,12 @@ public class UtilDate {
 	public static LocalDate stringToDate(String data, DateTimeFormatter formatter) {
 		return LocalDate.parse(data, formatter);
 	}
-
-
+	
+	public static LocalTime stringToTime(String time) {
+		return LocalTime.parse(time);
+	}
+	
+	public static Period calculateAge(LocalDate date) {
+		return Period.between(date, LocalDate.now());
+	}
 }
--- src/test/java/br/com/devmos/util/UtilDateTest.java
+++ src/test/java/br/com/devmos/util/UtilDateTest.java
@@ -1,8 +1,11 @@
 package br.com.devmos.util;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 import javax.swing.text.DateFormatter;
@@ -13,6 +16,7 @@ public class UtilDateTest {
 
 	private static final String STRING_DATE = "06-09-2021";
 	private static final String STRING_DATE_ISO = "2021-09-06";
+	private static final String STRING_TIME = "11:29:11";
 
 	@Test
 	public void testStringToDateOk() {
@@ -31,5 +35,22 @@ public class UtilDateTest {
 		assertEquals(6, date.getDayOfMonth());
 		assertEquals(9, date.getMonth().getValue());
 		assertEquals(2021, date.getYear());
+	}
+	
+	@Test
+	public void testStringToTime() {
+		LocalTime time = UtilDate.stringToTime(STRING_TIME);
+		
+		assertEquals(11, time.getHour());
+		assertEquals(29, time.getMinute());
+		assertEquals(11, time.getSecond());
+	}
+	
+	@Test
+	public void testCalculateAge() {
+		LocalDate date = LocalDate.of(1988, 12, 11);
+		Period age = UtilDate.calculateAge(date);
+		
+		assertNotNull(age);
 	}
 }
