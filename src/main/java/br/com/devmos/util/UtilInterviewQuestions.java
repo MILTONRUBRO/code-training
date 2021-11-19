37215cc9af31d35d2233c8f020e9ac9b442f23e5
Milton <miltonrubro@gmail.com>
2021-11-19 19:50:16 -0300

    count possible ways to construct building

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -349,5 +349,27 @@ public class UtilInterviewQuestions {
 		}
 		return pairs;
 	}
+	
+	public static int countWays(int n) {
+		if(n == 1)
+			return 4;
+		
+		int countB = 1;
+		int countS = 1;
+		int prevCountB;
+		int prevCountS;
+		
+		for(int i = 2; i <= n; i++) {
+			prevCountB = countB;
+			prevCountS = countS;
+			
+			countS = prevCountB + prevCountS;
+			countB = prevCountS;
+		}
+		
+		int result = countS + countB;
+		
+		return (result * result);
+	}
 
 }
