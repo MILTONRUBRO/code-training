48e1a6f1c422b13e70317a1e6c183241e2fb0865
Milton <miltonrubro@gmail.com>
2021-11-30 21:46:30 -0300

    metodo para reverter um array

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -395,5 +395,26 @@ public class UtilInterviewQuestions {
 		
 		return calcBinominalCoeficiente(n - 1, k -1) + calcBinominalCoeficiente(n - 1, k);
 	}
+	
+	public static int[] reverseArrayInGrups(int arr[], int n, int k){
+		for(int i = 0; i < n; i += k) {
+			
+			int left = i;
+			int right = Math.min(i + k - 1, n - 1);
+			int temp;
+			
+			while(left < right) {
+				temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+				
+				left += 1;
+				right -= 1;
+			}
+		}
+		
+		return arr;
+	}
+
 
 }
