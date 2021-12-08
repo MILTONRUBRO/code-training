be4f9da792013f4284220f2059d3bf4cdc8f8422
Milton <miltonrubro@gmail.com>
2021-12-07 22:10:52 -0300

    Metodo para contagem de vogais numa palavra

--- src/main/java/br/com/devmos/util/UtilString.java
+++ src/main/java/br/com/devmos/util/UtilString.java
@@ -90,4 +90,21 @@ public class UtilString {
 	public static String repeatStringWithSpace(String str, int tam) {
 		 return str.repeat(tam);
 	}
+	
+	public static int countVowels(String str) {
+		int count  = 0;
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(isVowel(str.charAt(i))) {
+				++count;
+			}
+		}
+		return count;
+	}
+
+	private static boolean isVowel(char ch) {
+		ch = Character.toUpperCase(ch);
+		
+		return (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+	}
 }
