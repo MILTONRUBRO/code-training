19a6b059c37369a97e6cc455b75c1bbd40d346a2
Milton <miltonrubro@gmail.com>
2022-02-14 20:46:30 -0300

    Flipar a matrix para o maior numero ficar no quadrante a esquerda

--- src/main/java/br/com/devmos/util/UtilNumbers.java
+++ src/main/java/br/com/devmos/util/UtilNumbers.java
@@ -169,5 +169,22 @@ public class UtilNumbers {
     public static double calcHipotenusa(double x, double y) {
     	return Math.hypot(x, y);
     }
+    
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+        int sum = 0;
+        int n = matrix.size()/2;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int num1 = matrix.get(i).get((2*n) - j - 1);
+                int num2 = matrix.get(i).get(j);
+                int num3 = matrix.get((2*n) - i - 1).get(j);
+                int num4 = matrix.get((2*n) - i - 1).get((2*n) - j - 1);
+
+                sum += Math.max(num1, Math.max(num2, Math.max(num3, num4)));
+            }
+          }
+          return sum;
+    }
 }
 
