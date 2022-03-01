c09352b191ea8dcc10f15cbc959cb98537ae68b0
Milton <miltonrubro@gmail.com>
2022-02-28 21:01:38 -0300

    metodo calcular hora de atraso

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -581,5 +581,19 @@ public class UtilInterviewQuestions {
     	
     	return (minCount == 1 || (maxCount == 1 && max == min+1)) ? "YES" : "NO";
     }
+    
+    public static void sundayMorning(String str) {
+    	
+		String[] relogio = str.split(":");    		
+		int hora = Integer.parseInt(relogio[0]);
+		int min = Integer.parseInt(relogio[1]);
+		switch (hora) {
+			case(7): System.out.println("Atraso maximo: " + min); break;
+			case(8): System.out.println("Atraso maximo: " + (60 + min)); break;
+			case(9): System.out.println("Atraso maximo: " + (120 + min)); break;
+			default: System.out.println("Atraso maximo: 0");
+		}
+
+    }
 
 }
