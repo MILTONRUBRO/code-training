fa12ccd021eeacd7f1176fbc3a7660ad4c861796
Milton <miltonrubro@gmail.com>
2022-03-07 20:58:42 -0300

    Metodo para gerar criptografia com RA

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -612,5 +612,30 @@ public class UtilInterviewQuestions {
     	
     	return fase;
     }
+    
+    public static String getPasswordRa(String frase) {
+		
+    	StringBuilder crypt = new StringBuilder();
+    	
+    	frase = frase.replaceAll(" ", "").trim();
+		
+		for(int i = 0; i < frase.length(); i++) {
+			
+			char c = frase.charAt(i);
+			
+			if (crypt.length() == 12) break;
+			if (c == 'G' || c == 'Q' || c == 'a' || c == 'k' || c == 'u')                  crypt.append('0');
+			else if (c == 'I' || c == 'S' || c == 'b' || c == 'l' || c == 'v')             crypt.append('1');
+			else if (c == 'E' || c == 'O' || c == 'Y' || c == 'c' || c == 'm' || c == 'w') crypt.append('2');
+			else if (c == 'F' || c == 'P' || c == 'Z' || c == 'd' || c == 'n' || c == 'x') crypt.append('3');
+			else if (c == 'J' || c == 'T' || c == 'e' || c == 'o' || c == 'y')             crypt.append('4');
+			else if (c == 'D' || c == 'N' || c == 'X' || c == 'f' || c == 'p' || c == 'z') crypt.append('5');
+			else if (c == 'A' || c == 'K' || c == 'U' || c == 'g' || c == 'q')             crypt.append('6');
+			else if (c == 'C' || c == 'M' || c == 'W' || c == 'h' || c == 'r')             crypt.append('7');
+			else if (c == 'B' || c == 'L' || c == 'V' || c == 'i' || c == 's')             crypt.append('8');
+			else if (c == 'H' || c == 'R' || c == 'j' || c == 't')                         crypt.append('9');
+		}
+		return crypt.toString();
+    }
 
 }
