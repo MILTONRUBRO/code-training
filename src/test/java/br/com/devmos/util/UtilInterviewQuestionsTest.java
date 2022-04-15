4138dfac1109e61007b4132dab23ab2791f0414b
Milton <miltonrubro@gmail.com>
2022-04-15 19:08:03 -0300

    Teste remover nullos de uma lista

--- src/test/java/br/com/devmos/util/UtilInterviewQuestionsTest.java
+++ src/test/java/br/com/devmos/util/UtilInterviewQuestionsTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -213,4 +214,12 @@ public class UtilInterviewQuestionsTest {
 		int asc = UtilInterviewQuestions.getAsc('A');
 		assertEquals(65, asc);
 	}
+	
+	@Test
+	public void testRemoveNulls() {
+		List<Object> names = Arrays.asList("ana", null, "jose");
+		List<Object> list = UtilInterviewQuestions.removeNulls(names);
+		assertEquals(2, list.size());
+	}
+
 }
