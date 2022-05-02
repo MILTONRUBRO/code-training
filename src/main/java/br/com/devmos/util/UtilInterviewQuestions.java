1a1f39ed8d85b5a3c4d8789c36c58736dd6e2391
Milton <miltonrubro@gmail.com>
2022-05-02 19:11:30 -0300

    Criando conversos em csv

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -803,5 +803,20 @@ public class UtilInterviewQuestions {
 	    
 	    return true;
 	}
+	
+	public String convertToCSV(String[] data) {
+	    return Stream.of(data)
+	      .map(this::escapeSpecialCharacters)
+	      .collect(Collectors.joining(","));
+	}
+	
+	public String escapeSpecialCharacters(String data) {
+	    String escapedData = data.replaceAll("\\R", " ");
+	    if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+	        data = data.replace("\"", "\"\"");
+	        escapedData = "\"" + data + "\"";
+	    }
+	    return escapedData;
+	}
 
 }
