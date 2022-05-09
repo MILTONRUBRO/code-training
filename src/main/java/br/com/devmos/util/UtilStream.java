8ddf553df4ff66123529265f0324e04afa9dd724
Milton <miltonrubro@gmail.com>
2022-05-09 19:18:11 -0300

    Usando collectors to map numa lista de objetos

---
+++ src/main/java/br/com/devmos/util/UtilStream.java
@@ -0,0 +1,15 @@
+package br.com.devmos.util;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import br.com.devmos.model.Book;
+
+public class UtilStream {
+	
+	public Map<String, String> listToMap(List<Book> books) {
+	    return books.stream().collect(Collectors.toMap(Book::getIsbn, Book::getName));
+	}
+
+}
