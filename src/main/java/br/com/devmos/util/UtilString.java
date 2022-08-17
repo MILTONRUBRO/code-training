a98ffbb5f47090e462598be66616791fb5dd76c4
Milton <miltonrubro@gmail.com>
2022-08-17 20:30:56 -0300

    trim all white space

--- src/main/java/br/com/devmos/util/UtilString.java
+++ src/main/java/br/com/devmos/util/UtilString.java
@@ -274,5 +274,18 @@ public class UtilString {
         }
         return false;
    }
+    
+    public static String trimAllWhitespace(String str) {
+        
+        StringBuilder sb = new StringBuilder(str.length());
+        
+        for (int i = 0; i < str.length(); i++) {
+             char c = str.charAt(i);
+             if (!Character.isWhitespace(c)) {
+                 sb.append(c);
+             }
+         }
+         return sb.toString();
+    }
 
 }
