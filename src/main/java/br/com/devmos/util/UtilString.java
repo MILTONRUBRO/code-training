0accddf1eeee279a18fce512b671fe34fa559f75
Milton <miltonrubro@gmail.com>
2022-08-24 21:01:18 -0300

    is blank in final

--- src/main/java/br/com/devmos/util/UtilString.java
+++ src/main/java/br/com/devmos/util/UtilString.java
@@ -312,5 +312,18 @@ public class UtilString {
 	        return str.toLowerCase().endsWith(suffix.toLowerCase());
 	    }
 	}
+	
+	public static boolean isBlank(final CharSequence cs) {
+	    int strLen;
+	    if (cs == null || (strLen = cs.length()) == 0) {
+	         return true;
+	    }
+	    for (int i = 0; i < strLen; i++) {
+	         if (!Character.isWhitespace(cs.charAt(i))) {
+	             return false;
+	         }
+	    }
+	    return true;
 
+	}
 }
