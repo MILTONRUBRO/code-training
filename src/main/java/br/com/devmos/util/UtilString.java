8a530e499083118e049b0286b3862e803d3abb37
Milton <miltonrubro@gmail.com>
2022-08-31 21:08:40 -0300

    check if is numeric

--- src/main/java/br/com/devmos/util/UtilString.java
+++ src/main/java/br/com/devmos/util/UtilString.java
@@ -342,4 +342,19 @@ public class UtilString {
 	     }
 	     return false;
 	}
+	
+	public static boolean isNumeric(final CharSequence cs) {
+	    if (isEmpty(cs)) {
+	         return false;
+	    }
+	    final int sz = cs.length();
+	    for (int i = 0; i < sz; i++) {
+	         if (!Character.isDigit(cs.charAt(i))) {
+	              return false;
+	        }
+	    }
+	    return true;
+	}
+	
+
 }
