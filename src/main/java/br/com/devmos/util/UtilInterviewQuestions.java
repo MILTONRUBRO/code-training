d360a566a147ccad9e24166063facc9a565da809
Milton <miltonrubro@gmail.com>
2022-10-27 22:23:42 -0300

    is palindrome using stringbuilder

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -1083,5 +1083,12 @@ public class UtilInterviewQuestions {
         Node left; 
         Node right;
     }
+    
+    public boolean isPalindromeUsingStringBuilder(String text) {
+        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        StringBuilder plain = new StringBuilder(clean);
+        StringBuilder reverse = plain.reverse();
+        return (reverse.toString()).equals(clean);
+    }
 
 }
