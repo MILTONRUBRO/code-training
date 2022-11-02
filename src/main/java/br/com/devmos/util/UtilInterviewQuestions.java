0a15ca335c71cebc2dd22c1c8af9ac80b0f2c7e8
Milton <miltonrubro@gmail.com>
2022-11-01 22:28:18 -0300

    is palindrome

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -1100,5 +1100,19 @@ public class UtilInterviewQuestions {
         }
         return (reverse.toString()).equals(clean);
     }
+    
+    public boolean isPalindrome(String text) {
+        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        int length = clean.length();
+        int forward = 0;
+        int backward = length - 1;
+        while (backward > forward) {
+            char forwardChar = clean.charAt(forward++);
+            char backwardChar = clean.charAt(backward--);
+            if (forwardChar != backwardChar)
+                return false;
+        }
+        return true;
+    }
 
 }
