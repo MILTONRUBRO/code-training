88504e456d5a8222613a1ecc174ba05e39dfe67f
Milton <miltonrubro@gmail.com>
2022-12-02 19:48:07 -0300

    find element in matrix

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -1191,5 +1191,22 @@ public class UtilInterviewQuestions {
 		int[] genders = {girls, boys};
 		return genders;
 	}
+	
+	public static boolean findElement(int[][] matrix, int elem) {
+		int row = 0;
+		int col = matrix[0].length - 1;
+		
+		while(row < matrix.length && col >= 0) {
+			if(matrix[row][col] == elem) {
+				return true;
+			}else if(matrix[row][col] > elem) {
+				col--;
+			}else {
+				row++;
+			}
+		}
+		
+		return false;
+	}
 
 }
