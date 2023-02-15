ad90336bbd5779d7037528dcca7c3e20dcf9124e
Milton <miltonrubro@gmail.com>
2023-02-14 22:17:51 -0300

    prototipe move disks

--- src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
+++ src/main/java/br/com/devmos/util/UtilInterviewQuestions.java
@@ -1354,5 +1354,14 @@ public class UtilInterviewQuestions {
 		return responsaveis.getOrDefault(key, "");
 		
 	}
+	
+	public void moveDisks(int n, Tower destination, Tower buffer) {
+		
+		if(n > 0) {
+			moveDisks(n - 1, buffer, destination);
+			moveTopTo(destination);
+			buffer.moveDisks(n - 1, destination, this);
+		}
+	}
 
 }
