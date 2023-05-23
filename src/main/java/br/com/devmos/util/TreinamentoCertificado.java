e41c20b7fc8aec604cfded9a72721964c1466f31
Milton <miltonrubro@gmail.com>
2023-05-22 21:55:22 -0300

    treinamento certificacão

---
+++ src/main/java/br/com/devmos/util/TreinamentoCertificado.java
@@ -0,0 +1,12 @@
+package br.com.devmos.util;
+
+public class TreinamentoCertificado {
+	
+	public static void treinoCertificacao() {
+		
+		for(int i = 0; i < 20; i++)
+			System.out.println(i);
+
+	}
+
+}
---
+++ src/test/java/br/com/devmos/util/TreinamentoCertificadoTest.java
@@ -0,0 +1,15 @@
+package br.com.devmos.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TreinamentoCertificadoTest {
+
+	@Test
+	void test() {
+		
+		TreinamentoCertificado.treinoCertificacao();
+	}
+
+}
